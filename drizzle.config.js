9e63d7af9c195fc4bf66cfc20b91414baf818315
VinitDhapodkar <vinitdhapodkar86@gmail.com>
2024-11-09 13:16:29 +0530

    first commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./src/utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:tDqMg0AR8HQX@ep-falling-sunset-a5zmc6mx.us-east-2.aws.neon.tech/beat_cancer?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:tDqMg0AR8HQX@ep-falling-sunset-a5zmc6mx.us-east-2.aws.neon.tech/beat_cancer?sslmode=require",
+  },
+};
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <PrivyProvider
+    appId="cm367jwfg041zeu9kjh0f8txn"
+    config={{
+      appearance: {
+        theme: "dark",
+      },
+      embeddedWallets: {
+        createOnLogin: "users-without-wallets",
+      },
+    }}
+  >
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </PrivyProvider>,
+);
